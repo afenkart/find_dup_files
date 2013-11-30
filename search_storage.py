ac5a673034a8fee551b3f722232167704f42f40b
Andreas Fenkart <andreas.fenkart@dev.digitalstrom.org>
2013-11-30 13:53:13 +0100

    bug: exclude special files during index.
    Signed-off-by: Andreas Fenkart <andreas.fenkart@dev.digitalstrom.org>

--- search_storage.py
+++ search_storage.py
@@ -9,12 +9,21 @@ import string
 import md5
 import traceback
 import socket
+import logging
+
+
+TIMESTMP_FMT = ("%H:%M:%S") # no %f for msecs
+FORMAT = '%(asctime)s.%(msecs)03d %(name)-5s %(message)s'
+logging.basicConfig(format=FORMAT, datefmt=TIMESTMP_FMT, level=logging.INFO)
 
 
 db = Storage(memory=False)
 db.recreate()
 
+visit_log = open('visit.log', 'w', buffering = 0)
+
 class FindFiles:
+    log = logging.getLogger("FindFiles")
 
     def sha1(self, name):
         ret = subprocess.check_output(["/usr/bin/sha1sum", name],
@@ -23,6 +32,7 @@ class FindFiles:
 
 
     def visit(self, path):
+        log = self.log
         problem_files = []
         for root, dirs, files in os.walk(path):
             if '.git' in dirs:
@@ -30,19 +40,36 @@ class FindFiles:
             if '.svn' in dirs:
                 dirs.remove('.svn')
             for f in files:
+                visit_log.write(("%s/%s\n" % (root, f)))
                 full = os.path.join(root, f)
+
                 if os.path.islink(full):
-                    #print "ignore link %s" % full
+                    # if link is invalid os.stat fails
+                    log.info("ignore symbolic link: %s" % full)
                     continue
+
+                mode = os.stat(full).st_mode
+                if stat.S_ISSOCK(mode):
+                    log.info("ignore socket: %s", full)
+                    continue
+                elif stat.S_ISCHR(mode):
+                    log.info("ignore character special device file: %s" % full)
+                    continue
+                elif stat.S_ISBLK(mode):
+                    log.info("ignore block special device file: %s" % full)
+                    continue
+                elif stat.S_ISFIFO(mode):
+                    log.info("ignore FIFO (named pipe): %s" % full)
+                    continue
+                elif stat.S_ISLNK(mode):
+                    log.info("ignore symbolic link: %s" % full)
+                    continue
+                assert(stat.S_ISREG(mode))
+
                 try:
                     sha1 = self.sha1(full)
                     db.add_file(sha1, full)
                 except subprocess.CalledProcessError, e:
-                    mode = os.stat(full).st_mode
-                    if stat.S_ISSOCK(mode):
-                        print "ignore sock", full
-                        continue
-
                     print "problem file", full
                     problem_files.append(full)
 
@@ -66,7 +93,6 @@ def unit_test():
     os.unlink('test-files/test-socket')
 
 unit_test()
-#sys.exit(-1)
 
 
 problem_files = find.visit('/home/afenkart')
