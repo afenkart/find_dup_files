ddb82ca0dc238771141d14055ba7a6d3576225c0
Andreas Fenkart <andreas.fenkart@dev.digitalstrom.org>
2013-12-29 12:30:16 +0100

    switch from sha1 to crc32.
    Signed-off-by: Andreas Fenkart <andreas.fenkart@dev.digitalstrom.org>

--- search_storage.py
+++ search_storage.py
@@ -4,7 +4,6 @@
 Index files in subfolder
 """
 
-
 import os, stat
 import subprocess
 import socket
@@ -21,10 +20,19 @@ VISIT_LOG = open('visit.log', 'w', buffering = 0)
 def sha1(name):
     """
     Does not belong here
+    1.4GB in 3.8 sec
     """
     ret = subprocess.check_output(["/usr/bin/sha1sum", name],
                                  stderr=subprocess.STDOUT)
     return str(ret).split(' ')[0]
+
+def crc32(name):
+    """
+    1.4GB in 1.9 sec
+    """
+    ret = subprocess.check_output(["/usr/bin/crc32", name],
+                                 stderr=subprocess.STDOUT)
+    return str(ret)
 
 
 class FindFiles:
@@ -57,10 +65,11 @@ class FindFiles:
                          mtime)
 
         try:
-            _sha1 = sha1(full_name)
+            _sha1 = "no-sha1" #sha1(full_name)
+            _crc32 = crc32(full_name)
             # stat.n_link
             dbs.add_inode(_stat.st_dev, _stat.st_ino, _stat.st_mtime,
-                          _stat.st_size, _sha1)
+                          _stat.st_size, _crc32, _sha1)
             return
         except subprocess.CalledProcessError:
             print "problem file", full_name
@@ -170,9 +179,8 @@ if __name__ == "__main__":
     unit_test()
     #os.abort()
 
-
     DB2 = Storage(memory=False)
-    #DB2.recreate()
+    DB2.recreate() # TODO
 
     __ITER__ = FindFiles(DB2)
     PROBLEM_FILES = __ITER__.search('/home/afenkart')
--- storage.py
+++ storage.py
@@ -81,19 +81,20 @@ class Storage:
                            (dev, inode))
         return cur.fetchone()
 
-    def add_inode(self, dev, inode, mtime, size, sha1):
+    def add_inode(self, dev, inode, mtime, size, crc32, sha1):
         """
         add dev/inode tuple
         """
         try:
             cur = self.con.cursor()
             if self.lookup_inode(dev, inode):
-                cur.execute("UPDATE Inodes SET st_mtime = ?, sha1 = ? WHERE \
-                            st_dev = ? and st_inode = ?",
-                            (mtime, sha1, dev, inode))
+                cur.execute("UPDATE Inodes SET st_mtime = ?, crc32 = ?, \
+                                    sha1 = ? \
+                            WHERE  st_dev = ? and st_inode = ?",
+                            (mtime, crc32, sha1, dev, inode))
             else:
                 cur.execute("INSERT INTO Inodes VALUES(NULL, ?, ?, ?, ?, ?, ?)",
-                            (dev, inode, mtime, size, "no-crc32", sha1))
+                            (dev, inode, mtime, size, crc32, sha1))
             self.con.commit()
         except lite.Error, err:
             print "Error %s: inode: %s--" % (err.args[0], inode)
@@ -109,21 +110,21 @@ class Storage:
         cur = self.con.cursor()
         cur.execute("DROP TABLE IF EXISTS Duplicates")
         cur.execute("DROP TABLE IF EXISTS Tmp")
-        cur.execute("CREATE TABLE Tmp(sha1 INTEGER, count INTEGER)")
+        cur.execute("CREATE TABLE Tmp(crc32 INTEGER, count INTEGER)")
         cur.execute("CREATE TABLE Duplicates (st_dev INTEGER, \
                     st_inode INTEGER, count INTEGER)")
         # Inodes contains no hard links, returns real double disk usage
-        cur.execute("INSERT INTO Tmp(sha1, count) \
-                     SELECT sha1, COUNT(*) Count \
+        cur.execute("INSERT INTO Tmp(crc32, count) \
+                     SELECT crc32, COUNT(*) Count \
                      FROM Inodes \
-                     GROUP BY sha1 \
+                     GROUP BY crc32 \
                      HAVING COUNT(*) > 1 \
                      ORDER BY COUNT(*) DESC")
         cur.execute("INSERT INTO Duplicates(st_dev, st_inode, count) \
                      SELECT st_dev, st_inode, tmp.Count \
                      FROM Inodes \
                      JOIN Tmp \
-                     ON Inodes.sha1=Tmp.sha1")
+                     ON Inodes.crc32=Tmp.crc32")
 
         return cur.execute("SELECT Duplicates.count, Inodes.sha1, \
                            Inodes.crc32, Files.st_dev, Files.st_inode, \
@@ -136,7 +137,7 @@ class Storage:
                            ON Files.st_dev=Inodes.st_dev AND \
                               Files.st_inode = Inodes.st_inode \
                            ORDER BY Duplicates.count, Inodes.sha1, \
-                                    Inodes.st_size, Files.Name")
+                                    Inodes.crc32, Inodes.st_size, Files.Name")
 
     def remove(self, sha1, name):
         """
