be0d209453762ffb8ee634d594e1da38f07dfef1
Andreas Fenkart <andreas.fenkart@dev.digitalstrom.org>
2013-12-29 12:04:21 +0100

    pylint storage file.
    Signed-off-by: Andreas Fenkart <andreas.fenkart@dev.digitalstrom.org>

--- storage.py
+++ storage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+
+"""
+Map storage/retrieval needs to sqlite
+"""
+
 import sqlite3 as lite
-import sys, os
-import subprocess
-import string
 import traceback
 import logging
 
@@ -11,6 +13,9 @@ FORMAT = '%(asctime)s.%(msecs)03d %(name)-5s %(message)s'
 logging.basicConfig(format=FORMAT, datefmt=TIMESTMP_FMT, level=logging.INFO)
 
 class Storage:
+    """
+    Do it all class
+    """
     def __init__(self, memory, filename = "files.db"):
         self.log = logging.getLogger("Storage")
 
@@ -22,9 +27,15 @@ class Storage:
         #self.con.text_factory = str
 
     def recreate(self):
+        """
+        TODO drop
+        """
         self.create_db()
 
     def create_db(self):
+        """
+        recreates files/inodes table
+        """
         cur = self.con.cursor()
         cur.execute("DROP TABLE IF EXISTS Files")
         cur.execute("DROP TABLE IF EXISTS Inodes")
@@ -34,9 +45,12 @@ class Storage:
         cur.execute("CREATE TABLE Inodes(id INTEGER PRIMARY KEY, \
                     st_dev INTEGER, st_inode INTEGER, st_mtime FLOAT, \
                     st_size INTEGER, CRC32 Text, SHA1 Text)")
-        self.con.commit();
+        self.con.commit()
 
     def add_file(self, name, dev, inode):
+        """
+        filename -> dev/inode connections
+        """
         log = self.log
         try:
             cur = self.con.cursor()
@@ -48,45 +62,56 @@ class Storage:
                         (name.decode('utf-8'), dev, inode))
             else:
                 log.debug("Existing file name %r\n", name)
-            self.con.commit();
-        except lite.Error, e:
-            print "Error %s: name: %s" % (e.args[0], name)
+            self.con.commit()
+        except lite.Error, err:
+            print "Error %s: name: %s" % (err.args[0], name)
             print traceback.format_exc()
             self.con.rollback()
-        except UnicodeDecodeError, e:
+        except UnicodeDecodeError, err:
             print traceback.format_exc()
             print name
 
 
     def lookup_inode(self, dev, inode):
+        """
+        check if dev/inode tuple exists
+        """
         cur = self.con.cursor()
         cur.execute("SELECT * FROM Inodes WHERE st_dev = ? and st_inode = ?",
                            (dev, inode))
         return cur.fetchone()
 
     def add_inode(self, dev, inode, mtime, size, sha1):
+        """
+        add dev/inode tuple
+        """
         try:
             cur = self.con.cursor()
             if self.lookup_inode(dev, inode):
                 cur.execute("UPDATE Inodes SET st_mtime = ?, sha1 = ? WHERE \
-                            st_dev = ? and st_inode = ?", (mtime, sha1, dev, inode))
+                            st_dev = ? and st_inode = ?",
+                            (mtime, sha1, dev, inode))
             else:
                 cur.execute("INSERT INTO Inodes VALUES(NULL, ?, ?, ?, ?, ?, ?)",
                             (dev, inode, mtime, size, "no-crc32", sha1))
-            self.con.commit();
-        except lite.Error, e:
-            print "Error %s: inode: %s--" % (e.args[0], inode)
+            self.con.commit()
+        except lite.Error, err:
+            print "Error %s: inode: %s--" % (err.args[0], inode)
             self.con.rollback()
             return False
 
 
     def duplicates(self): # filter by size / number of duplicates
+        """
+        Find inodes with equal crc32/sha1, but different st_dev/st_inode
+        and all filenames, including hard links
+        """
         cur = self.con.cursor()
         cur.execute("DROP TABLE IF EXISTS Duplicates")
         cur.execute("DROP TABLE IF EXISTS Tmp")
         cur.execute("CREATE TABLE Tmp(sha1 INTEGER, count INTEGER)")
-        cur.execute("CREATE TABLE Duplicates (st_dev INTEGER, st_inode INTEGER, \
-                    count INTEGER)")
+        cur.execute("CREATE TABLE Duplicates (st_dev INTEGER, \
+                    st_inode INTEGER, count INTEGER)")
         # Inodes contains no hard links, returns real double disk usage
         cur.execute("INSERT INTO Tmp(sha1, count) \
                      SELECT sha1, COUNT(*) Count \
@@ -100,8 +125,9 @@ class Storage:
                      JOIN Tmp \
                      ON Inodes.sha1=Tmp.sha1")
 
-        return cur.execute("SELECT Duplicates.count, Inodes.sha1, Inodes.crc32, \
-                           Files.st_dev, Files.st_inode, Files.Name  \
+        return cur.execute("SELECT Duplicates.count, Inodes.sha1, \
+                           Inodes.crc32, Files.st_dev, Files.st_inode, \
+                           Files.Name  \
                            FROM Files \
                            JOIN Duplicates \
                            ON Files.st_dev=Duplicates.st_dev AND \
@@ -112,47 +138,36 @@ class Storage:
                            ORDER BY Duplicates.count, Files.Name")
 
     def remove(self, sha1, name):
+        """
+        non-functional
+        """
         try:
             cur = self.con.cursor()
-            cur.execute("DELETE FROM Files WHERE sha1 = ? and Name = ?", (sha1, name))
-            self.con.commit();
-        except lite.Error, e:
-            print "Error %s: name: %s" % (e.args[0], name)
+            cur.execute("DELETE FROM Files WHERE sha1 = ? and Name = ?",
+                        (sha1, name))
+            self.con.commit()
+        except lite.Error, err:
+            print "Error %s: name: %s" % (err.args[0], name)
             self.con.rollback()
 
 
 
-def build_test_corpus(db):
-    print "test_corpus"
-    db.add_file('9db39b5c8b9eb70149801f8c9112c3ef50dcd562', 'ida_-_Vylet_na_prehradu/IMAG0167.jpg')
-    db.add_file('9db39b5c8b9eb70149801f8c9112c3ef50dcd562', 'et_na_prehradu/IM.jpg')
-    db.add_file('9db39b5c8b9eb70149801f8c9112c3ef50dcd562', '_na_prehradu/IM.jpg')
-    db.add_file('9db39b5c8b9eb70149801f8c9112c3ef50dcd565', 'erehradu/IM.jpg')
-    db.add_file('881d34fd4fc2d55af571e9f8c587108bf4d45ea2','cii_Empik_hleda_Foxika/P1080538.jpg')
-    db.add_file('881d34fd4fc2d55af571e9f8c587108bf4d45ea2','i_Empik_hleda_Foxika/P1080538.jpg')
-    db.add_file('881d34fd4fc2d55af571e9f8c587108bf4d45ea5','i_E_Foxika/P1080538.jpg')
-    db.add_file('881d34fd4fc2d55af571e9f8c587108bf4d45ea2','i_Empik_hledaFika/P1080538.jpg')
-    db.add_file('881d34fd4fc2d55af571e9f8c587108bf4d45ea2','i_Empik_hledaFika/P1080538.jpg')
-    db.add_file('881d34fd4fc2d55af571e9f8c587108bf4d45ea5','i_Foxika/P15038.jpg')
-    db.add_file('881d34fd4fc2d55af571e9f8c587108bf4d45ea5','i_Eoxika/P15038.jpg')
-
-
-if __name__ == "__main__":
-    db = Storage(memory=True)
-    db.recreate()
-    build_test_corpus(db)
-    #print db
-    #print len(db.all_rows())
+def unit_test():
+    """
+    unit test
+    """
+    dbm = Storage(memory=True)
+    dbm.recreate()
+    #build_test_corpus(dbm)
 
     print "\ncollisions"
-    for row in db.duplicates():
-        print row
-
-    print "\nfiles for one sha1"
-    for row in db.filenames('9db39b5c8b9eb70149801f8c9112c3ef50dcd562'):
+    for row in dbm.duplicates():
         print row
 
     print "\nremove one of above"
-    db.remove('9db39b5c8b9eb70149801f8c9112c3ef50dcd562', '_na_prehradu/IM.jpg')
-    for row in db.filenames('9db39b5c8b9eb70149801f8c9112c3ef50dcd562'):
+    for row in dbm.duplicates():
         print row
+
+
+if __name__ == "__main__":
+    unit_test()
